8625e0ff048bcef4e185eb16538272a9f645b022
Liane Bez <bez.lianealcantara@gmail.com>
2019-01-22 19:40:28 -0200

    [021] Desafio único

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 const $root = document.querySelector("#root");
+
 const $memoryCard = document.createElement("article");
-const $icon =
-  `<img 
-  src='img/icon-collabcode.png' 
-  alt='Gueio mascote da CollabCode' 
-  class='icon'
-  />`;
+const $memoryCardActive = document.createElement("article");
+
+const $icon = `<img 
+    src='img/icon-collabcode.png' 
+    alt='Gueio mascote da CollabCode' 
+    class='icon'
+    />`;
+const $iconActive = `<img
+    src="img/icon-c.png"
+    alt="Ícone C++" 
+    class="icon"
+    />`;
 
 $memoryCard.classList.add("memory-card");
-
 $root.insertBefore($memoryCard, null);
-
 $memoryCard.insertAdjacentHTML("afterbegin", $icon); //colocar o hmtl que está no $icon logo após começar o $memocyCard, que é o article
+
+$memoryCardActive.classList.add("active");
+
+$root.insertBefore($memoryCardActive, null);
+
+$memoryCardActive.insertAdjacentHTML("afterbegin", $iconActive);
