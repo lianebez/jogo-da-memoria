592741c155efd29df72c42a85982825aba9d2de2
Liane Bez <bez.lianealcantara@gmail.com>
2019-01-25 07:31:47 -0200

    [026] Desafio único

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -1,4 +1,47 @@
-const createMemoryCard = ({src, alt, nameClass}) => `
+const createMemoryCard = ({ src, alt, nameClass }) => {
+    const $head = document.querySelector("head");
+    const $style = document.createElement("style");
+
+    $style.textContent = `
+        .memory-card {
+        width: 155px;
+        height: 155px;
+        background-color: #f25a70;
+        border-radius: 30px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16);
+        position: relative;
+        cursor: pointer;
+        }
+
+        .memory-card.-front {
+        background-color: #fff;
+        }
+
+        .memory-card.-front::before {
+        content: "";
+        width: 95px;
+        height: 95px;
+        background-color: #d4d4d4;
+        border-radius: 50%;
+        position: absolute;
+        }
+
+        .memory-card > .icon {
+        width: 100px;
+        height: 100px;
+        }
+
+        .memory-card.-front > .icon {
+        position: absolute;
+        transform: translateY(-12px);
+        }
+    `
+    $head.insertBefore($style, null);
+
+    return `
     <article class="memory-card ${nameClass}">
         <img 
             src="${src}"
@@ -8,5 +51,13 @@ const createMemoryCard = ({src, alt, nameClass}) => `
         />
     </article>
 `;
+};
 
 const handlelClick = () => console.log("ae");
+
+// 0 - criar uma funcao createMemoryCard
+// 1 - pegar a header (querySelector)
+// 2 - criar o elemento style (createElement)
+// 3 - colocar os estilos (textContent)
+// 4 - preciso dizer onde os estilos irão ficar (dentro he head antes de niguém no final) (insertBefore)
+// 5 - retornar o HMTL ou createMemoryCard?
